6888ef298dd7555fcfc3eb5df93ce31bf2e02340
Pratik Shinde <pratikshinde377@gmail.com>
2023-01-23 13:43:10 +0000

    Update 3 files

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
-# TMDB_UP_School React Movie App
-This is a movie app with using The Movie Database. Building with ReactJS.
+# React Movie App
 <h4>Used Technologies</h4>
-<li>React</li>
-<li>Bootstrap</li>
-<li>React Slick</li>
-<li>MUI Buttons</li>
-<li>Axios</li>
-<li>React Query</li>
-<hr/>
-<h4>API</h4>
-https://www.themoviedb.org/documentation/api
-<hr/>
-  <a href="https://tmdb-up-school-capstone.vercel.app/">Project Link 🎬🍿</a> 
+<li>React JS</li>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,53 +1,68 @@
-import { Link } from 'react-router-dom';
-import React, { useState } from 'react';
-import { render } from 'react-dom';
-
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { render } from "react-dom";
 
 function Navbar(props) {
-  const [searchStr, setSearchStr] = useState('')
+  const [searchStr, setSearchStr] = useState("");
   const handleSearch = (e) => {
     setSearchStr(e.target.value);
-  }
+  };
   const redirectTomovies = () => {
     window.location.href = "search?name=" + searchStr;
-  }
-
- 
+  };
 
   return (
     <>
-     <div className='container-fluid text-light' style={{ backgroundColor: "#1e272e" }}>
-      <div className="container  text-light">
-        <header className="p-3 mb-3 border-bottom">
-          <div className="container">
-            <div className="d-flex flex-wrap align-items-start justify-content-between">
-              <div className="d-flex link-light text-light">
-              <Link to="popular" className="nav-link px-2 link-light" style={{fontSize:"20px"}}>
-                  {' '}
-                  Movie<span style={{color:"yellow"}}>Db</span>
-                </Link>
-             
-                <Link to="popular" className="nav-link px-2 link-light">
-                  {' '}
-                  Popular
-                </Link>
-                <Link to="toprated" className="nav-link px-2 link-light">
-                  Top Rated
-                </Link>
-                <Link to="upcoming" className="nav-link px-2 link-light">
-                  Upcoming
-                </Link>
+      <div
+        className="container-fluid text-light"
+        style={{ backgroundColor: "#1e272e" }}
+      >
+        <div className="container  text-light">
+          <header className="p-3 mb-3 border-bottom">
+            <div className="container">
+              <div className="d-flex flex-wrap align-items-start justify-content-between">
+                <div className="d-flex link-light text-light">
+                  <Link
+                    to="popular"
+                    className="nav-link px-2 link-light"
+                    style={{ fontSize: "20px" }}
+                  >
+                    Movie<span style={{ color: "yellow" }}>Db</span>
+                  </Link>
 
-                <input type="text" onChange={(e) => handleSearch(e)} placeholder="Search.." />
-                <button style={{backgroundColor:"grey", border:"none",color:"white", padding :" 8px 18px", textAlign:"center"}} onClick={() => redirectTomovies()}>Search</button>
-         
-
+                  <Link to="popular" className="nav-link px-2 link-light">
+                    Popular
+                  </Link>
+                  <Link to="toprated" className="nav-link px-2 link-light">
+                    Top Rated
+                  </Link>
+                  <Link to="upcoming" className="nav-link px-2 link-light">
+                    Upcoming
+                  </Link>
+                  <br />
+                  <input
+                    type="text"
+                    onChange={(e) => handleSearch(e)}
+                    placeholder="Search.."
+                  />
+                  <button
+                    style={{
+                      backgroundColor: "grey",
+                      border: "none",
+                      color: "white",
+                      padding: " 8px 18px",
+                      textAlign: "center",
+                    }}
+                    onClick={() => redirectTomovies()}
+                  >
+                    Search
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        </header>
+          </header>
+        </div>
       </div>
-    </div>
     </>
   );
 }
